0ad7124db107d69ebc1048e8109041f15366d5a6
Amwatah Obuya <obuyaamwatah@gmail.com>
2023-09-20 12:17:30 +0300

    removed stripe

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
@@ -12,34 +12,23 @@ interface CourseEnrollButtonProps {
   courseId: string;
 }
 
-export const CourseEnrollButton = ({
-  price,
-  courseId,
-}: CourseEnrollButtonProps) => {
+export const CourseEnrollButton = ({ price, courseId }: CourseEnrollButtonProps) => {
   const [isLoading, setIsLoading] = useState(false);
 
   const onClick = async () => {
     try {
       setIsLoading(true);
-
-      const response = await axios.post(`/api/courses/${courseId}/checkout`)
-
-      window.location.assign(response.data.url);
+      const response = await axios.post(`/api/courses/${courseId}/checkout`);
     } catch {
       toast.error("Something went wrong");
     } finally {
       setIsLoading(false);
     }
-  }
+  };
 
   return (
-    <Button
-      onClick={onClick}
-      disabled={isLoading}
-      size="sm"
-      className="w-full md:w-auto"
-    >
+    <Button onClick={onClick} disabled={isLoading} size="sm" className="w-full md:w-auto">
       Enroll for {formatPrice(price)}
     </Button>
-  )
-}
+  );
+};
